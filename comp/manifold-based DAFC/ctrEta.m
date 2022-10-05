e137a185a7d1d9a202ddc1196166427412d155eb
Oliver <37354467+Dzalwaysc@users.noreply.github.com>
2022-10-05 12:43:42 +0800

    Update ctrEta.m

--- comp/manifold-based DAFC/ctrEta.m
+++ comp/manifold-based DAFC/ctrEta.m
@@ -1,4 +1,4 @@
-function [tao] = ctrEta(index, w, D, Eta, Etad, M__, C__, D__, loop)
+function [tao] = ctrEta(index, w, D, Eta, Etad, Etadd, M__, C__, D__, loop)
     epos = [0, 0]; eyaw = 0; eposd = [0, 0]; eyawd = 0;
     posdd = [0, 0]; yawdd = 0; alpha = 0;
     edge_ind = find(D(index,:)~=0);
@@ -13,9 +13,9 @@ function [tao] = ctrEta(index, w, D, Eta, Etad, M__, C__, D__, loop)
         eyaw = eyaw + Eta{index}(loop,3)-Eta{j}(loop,3);
         eyawd = eyawd + Etad{index}(loop,3)-Etad{j}(loop,3);
         
-        alpha = alpha + w(k);
-        posdd = posdd + w(k)*Eta{j}(loop,1:2);
-        yawdd = yawdd + Eta{j}(loop,3);
+        alpha = alpha - diag([w(k), w(k), 1]);
+        posdd = posdd + -w(k)*Etadd{j}(loop,1:2);
+        yawdd = yawdd + -1*Etadd{j}(loop,3);
     end
     k1 = diag([300,300,18]); h = 300;
     z = [epos, eyaw]; zd = [eposd, eyawd]; zdd = [posdd, yawdd];
@@ -26,7 +26,7 @@ function [tao] = ctrEta(index, w, D, Eta, Etad, M__, C__, D__, loop)
     D__ = reshape(D__{index}(loop,:), 3, 3);
     
     tao = Etad{index}(loop,:) * C__' + Etad{index}(loop,:) * D__' ...
-        + 1/alpha  * (zdd - h*s) * inv(M__)';
+        + (zdd + h*s) * M__' * inv(alpha);
 
     for i = 1:3
         if abs(tao(i))>500
